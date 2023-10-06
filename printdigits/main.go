55a83e7cfd83691fe2b7d73f5a3b10a41322540f
Leo1G2 <leo.gruel@ynov.com>
2023-10-06 11:39:53 +0200

    printdigits

--- printdigits/main.go
+++ printdigits/main.go
@@ -2,8 +2,8 @@ package main // main = dossier principal, branche principale
 
 import "github.com/01-edu/z01" // import de fonction ou de ressources git hub
 
-func main() { //func est toujours accompagné de () {}, et elle appelle toujours main 
-	for i := 48; i < 58; i++ { //i :=  pour definir variable, pour i < x la boucle continue jusqu'a atteindre x; i ++ = I+1
+func main() { // f unc est toujours accompagné de () {}, et elle appelle toujours main 
+	for i := 48; i < 58; i++ { // i :=  pour definir variable, pour i < x la boucle continue jusqu'a atteindre x; i ++ = I+1
 		z01.PrintRune(rune(i))
 	}
 	z01.PrintRune('\n')
