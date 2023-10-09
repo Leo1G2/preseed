79448ab342520cd7e3bed41ee27e9952a44d47ab
Leo1G2 <leo.gruel@ynov.com>
2023-10-09 15:30:45 +0200

    isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -4,11 +4,11 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	var i int
-	if i < 0 {
-		z01.PrintRune('T')
+	if i >= 0 {
+		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	} else {
-		z01.PrintRune('F')
+		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	}
 }
