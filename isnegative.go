c6b121bb14fe7b5aef1abaf211dc1bd96eda9bbf
Leo1G2 <leo.gruel@ynov.com>
2023-10-09 16:20:55 +0200

    isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	var i int
-	if i < 0 {
+if nb < 0 {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
+
 	} else {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
