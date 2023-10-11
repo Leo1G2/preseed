8b15a4eeff0461db76568aa14db958a812ae8ae6
Leo1G2 <leo.gruel@ynov.com>
2023-10-11 15:12:18 +0200

    printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -9,23 +9,22 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-
-				if i != k || j != l {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(32)
-					z01.PrintRune(k)
-					z01.PrintRune(l)
-					if i + k == j +l {
-						z01.PrintRune('\n')
-					} else {
-						z01.PrintRune(44)
+					if i != k || j != l {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						z01.PrintRune(32)
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if i + k == j +l {
+							z01.PrintRune('\n')
+						} else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
 
+						}
 					}
 				}
 			}
 		}
 	}
-}
 }
