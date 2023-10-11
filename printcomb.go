40cd45ca6f63a470240147b9e4046d4df2f83eef
Leo1G2 <leo.gruel@ynov.com>
2023-10-11 11:11:19 +0200

    printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -1,29 +1,28 @@
-package main
+package piscine
 
 import (
 	"github.com/01-edu/z01"
 	"github.com/01-edu/z01.PrintRune"
 )
 
+func PrintComb(nb int) {
 
-func PrintComb	(nb int){
-
-	for i := '0'; i <='7'; i++{
-		for j := i+1; j <='8'; j++{
-			for k := j+1; k <='0'; k++{
-				if i < j && j< k{
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '0'; k++ {
+				if i < j && j < k {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					if i=='7' && j=='8' && k=='9' {
+					if i == '7' && j == '8' && k == '9' {
 						z01.PrintRune('\n')
 					} else {
 						z01.PrintRune(44)
 						z01.PrintRune(32)
-						
+
 					}
 				}
 			}
 		}
-	}	
 	}
+}
