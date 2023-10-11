7e6067ee0261fba03ab4dcf6bca28c11d5314f9e
Leo1G2 <leo.gruel@ynov.com>
2023-10-11 11:02:10 +0200

    printcomb

---
+++ printcomb.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"github.com/01-edu/z01.PrintRune"
+)
+
+
+func PrintComb	(nb int){
+
+	for i := '0'; i <='7'; i++{
+		for j := i+1; j <='8'; j++{
+			for k := j+1; k <='0'; k++{
+				if i < j && j< k{
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i=='7' && j=='8' && k=='9' {
+						z01.PrintRune('\n')
+					} else {
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+						
+					}
+				}
+			}
+		}
+	}	
+	}
