456ba51b001baebda87cb27b527ad147da3517f3
Leo1G2 <leo.gruel@ynov.com>
2023-10-24 21:40:32 +0200

    marche zebi

--- strlen.go
+++ strlen.go
@@ -1,11 +1,11 @@
 package piscine
 
 func StrLen(s string) int {
-	x:=len(s)
-	for _, char:= range s {
-		if char == 'é' {
-		x = x-1
-		}
+	x := len(s)
+	for _, char := range s {
+			if char == 'é' {
+			x = x - 1
+			}
 	}
 	return x
 }
