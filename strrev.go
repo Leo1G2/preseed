990b569046395baf40b5b425856beb9355abc0d1
Leo1G2 <leo.gruel@ynov.com>
2023-10-26 10:36:14 +0200

    lezgongue

--- strrev.go
+++ strrev.go
@@ -1,8 +1,8 @@
 package piscine 
 
 func StrRev(s string) string {
-	r:=[]rune(s)
-	for i,j:=0,len(s)-1;i<j;i,j=i+1,j-1 {
+	r := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j= i+1, j-1 {
 		r[i],r[j]=r[j],r[i]
 	}
 	return string(r)
