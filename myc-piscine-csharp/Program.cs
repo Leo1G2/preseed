61ec7c3529f1b693c28bcd9e557539f2d0e1b9a2
Leo1G2 <leo.gruel@ynov.com>
2025-01-21 16:57:22 +0100

    first commit

--- myc-piscine-csharp/Program.cs
+++ myc-piscine-csharp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CSharpDiscovery.Quest03;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            var poi = new PointOfInterest();
+            Console.WriteLine(poi.Latitude);
+            Console.WriteLine(poi.Longitude);
+            Console.WriteLine(poi.Name);
+
+            Console.WriteLine(PointOfInterest.GoogleMapsUrlTemplate);
+        }
+    }
+}
